8161add1cc31240cc4afb0cdd6773086ac297dca
Abhinav Gairola <abhinuclear01@gmail.com>
2021-05-02 08:34:31 -0400

    Add files via upload

--- Scripts/Tweet_downloader.py
+++ Scripts/Tweet_downloader.py
@@ -1,24 +1,21 @@
-import datetime
-print(datetime.datetime.today())
-print(datetime.datetime(2021, 4, 12, 7, 39, 55, 173504).weekday())
-print(datetime.datetime.today().weekday())
 import twint
-
 import os
+
+if not os.path.exists('../Tweets_json'):
+  os.mkdir('../Tweets_json')
 
 
 with open('../Txt/Tickers.txt','r') as f:
   lines= (f.readlines())
 All_cashtags=['$'+x.replace('\n','') for x in lines]
-print(All_cashtags)
-ttt
+
 c = twint.Config()
-#c.Username = 
-for tags in All_cashtags[0:100]:
+
+for tags in All_cashtags:
     c.Search = tags 
     c.Since = '2016-01-01 8:00:00'
     c.Store_json=True
     c.Lang = 'en'
     c.Hide_output = True
-    c.Output = 'Tweets_json/Tweets'+tags+'.json'
+    c.Output = '../Tweets_json/Tweets'+tags+'.json'
     twint.run.Search(c)
